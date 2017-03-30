85585c893e5c1b3d358464907fa1fce98c583bd3
Javier Pollak <javi.po.123@gmail.com>
2017-03-30 18:43:18 -0400

    Add comments & Prettify code

--- interactive.py
+++ interactive.py
@@ -1,39 +1,62 @@
+import logging
 import shlex
 import Progs
+
+logger = logging.getLogger('Systhemer.interactive')
 
 
 class commands(object):
     @staticmethod
-    def quit(S):
+    def _quit(S):
         print('exiting interactive mode...')
         exit(0)
+
+    @staticmethod
+    def _exec(S, s):
+        print(s)
+        exec(s)
 
     @staticmethod
     def reload_progdefs(S):
         Progs.setup(S)
 
-    @staticmethod
-    def exec(S, s):
-        exec(s)
-
 
 def run(Settings):
     def run_cmd(cmd, args=[]):
         if args:
-            cmd(Settings, eval(', '.join(args)))
+            cmd(Settings, *args)
         else:
             cmd(Settings)
     try:
         while True:
+            # get input
             command = input('> ')
+
+            # skip empty line
             if not command:
                 continue
+
+            # parse input
             command = shlex.split(command)
-            print(command)
-            if command[0] in dir(commands):
-                run_cmd(eval('commands.' + command[0]), command[1:])
+            command, *args = command
+            logger.log(Settings.VDEBUG,
+                       'cmd: \'%s\', args: %s' % (command, args))
+
+            # replace '-' chars with '_' in command
+            command = ''.join([c if c != '-' else '_' for c in command])
+
+            # run command with args
+            # it tries to find the command in commands and then
+            # _command and if nothing is found then cmd=None
+            cmd = getattr(commands, command,
+                          getattr(commands, '_'+command,
+                                  None))
+            if cmd:
+                run_cmd(cmd, args)
             else:
                 print('Error: command not found!')
+
+    # handle KeyboardInterrupt
     except KeyboardInterrupt:
         print()
         run_cmd(commands.quit)
